0b811361740accaefa323b92d261ec1aaaba1502
walter.pace <44205750+wallytsx@users.noreply.github.com>
2020-04-17 19:43:38 -0300

    moved Theo forlder

--- Theo Leyenda/Source/BasicSystemWeapons/BasicSystemWeapons.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "BasicSystemWeapons.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BasicSystemWeapons, "BasicSystemWeapons" );
- 
