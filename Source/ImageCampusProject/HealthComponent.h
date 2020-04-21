4bf3d190ebc147b3332bec8f3b15b147af762fa7
walter.pace <44205750+wallytsx@users.noreply.github.com>
2020-04-20 23:16:58 -0300

    fix compilation

--- Source/ImageCampusProject/HealthComponent.h
+++ Source/ImageCampusProject/HealthComponent.h
@@ -7,7 +7,7 @@
 #include "HealthComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS()
 class IMAGECAMPUSPROJECT_API UHealthComponent : public UActorComponent
 {
 	GENERATED_BODY()
